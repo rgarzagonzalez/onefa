32edc75e18dcfa22fab0c5839dcc2691c0d0840f
rgarzagonzalez <ricardo@madhouse.com.mx>
2025-03-14 12:21:01 -0600

    Agregar render-build y variable de puerto

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require("express");
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get("/url",(req, res, next) => {
-    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food", "Cat"]);
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server running on port 3000");
 });
